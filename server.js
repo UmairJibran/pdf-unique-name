f22176980e28a4316fd68d11757ff2379fb2cb31
Umair Jibran <umairjibran7@gmail.com>
2020-09-29 01:48:11 +0500

    upadte id table

--- server.js
+++ server.js
@@ -2,21 +2,33 @@ const fs = require("fs");
 const path = require("path");
 const uuid = require("uuid");
 
-fs.readdir(path.join(__dirname, "certs"), (err, files) => {
-   let names = "";
+fs.readdir(path.join(__dirname, "certs"), async (err, files) => {
    if (err) return console.error(err);
+   await fs.appendFile(
+      path.join(__dirname, "certs", "ids.json"),
+      `{"ids":[`,
+      (err) => {
+         if (err) return console.error(err);
+      }
+   );
    for (let index = 0; index < files.length; index++) {
       let v4UUID = uuid.v4().replace(/-/g, "");
-      fs.rename(
+      await fs.rename(
          path.join(__dirname, "certs", files[index]),
          path.join(__dirname, "certs", `${v4UUID}.pdf`),
          (err) => {
             if (err) return console.error(err);
          }
       );
-      names += `${v4UUID}\n`;
+      await fs.appendFile(
+         path.join(__dirname, "certs", "ids.json"),
+         `"${v4UUID}",`,
+         (err) => {
+            if (err) return console.error(err);
+         }
+      );
    }
-   fs.writeFile(path.join(__dirname, "certs", "ids.txt"), names, (err) => {
+   fs.appendFile(path.join(__dirname, "certs", "ids.json"), `]}`, (err) => {
       if (err) return console.error(err);
    });
 });
