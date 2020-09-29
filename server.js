b31e82ee5de0706c2e1cfd1bc3c5a9e6ac59449c
Umair Jibra <umairjibran7@gmail.com>
2020-09-29 14:47:52 +0500

    sort name by old order

--- server.js
+++ server.js
@@ -2,33 +2,25 @@ const fs = require("fs");
 const path = require("path");
 const uuid = require("uuid");
 
-fs.readdir(path.join(__dirname, "certs"), async (err, files) => {
+fs.readdir(path.join(__dirname, "raw"), (err, files) => {
    if (err) return console.error(err);
-   await fs.appendFile(
-      path.join(__dirname, "certs", "ids.json"),
-      `{"ids":[`,
-      (err) => {
-         if (err) return console.error(err);
-      }
-   );
+   let names = [];
    for (let index = 0; index < files.length; index++) {
       let v4UUID = uuid.v4().replace(/-/g, "");
-      await fs.rename(
-         path.join(__dirname, "certs", files[index]),
+      names.push(v4UUID);
+      fs.rename(
+         path.join(__dirname, "raw", files[index]),
          path.join(__dirname, "certs", `${v4UUID}.pdf`),
          (err) => {
             if (err) return console.error(err);
          }
       );
-      await fs.appendFile(
-         path.join(__dirname, "certs", "ids.json"),
-         `"${v4UUID}",`,
-         (err) => {
-            if (err) return console.error(err);
-         }
-      );
    }
-   fs.appendFile(path.join(__dirname, "certs", "ids.json"), `]}`, (err) => {
-      if (err) return console.error(err);
-   });
+   fs.writeFile(
+      path.join(__dirname, "certs", "test.json"),
+      JSON.stringify(names),
+      (err) => {
+         if (err) return console.error(err);
+      }
+   );
 });
